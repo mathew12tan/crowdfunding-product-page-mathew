47e8a74f087f309384e58f06f2b7814fabdcc519
mathew tan <mathew12tan@gmail.com>
2021-07-11 21:50:14 +0800

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-// show mobile menu
 const navToggle = document.querySelector(".nav-toggle");
 const navDesktop = document.querySelector(".nav-desktop");
 
+const bookmark = document.querySelector(".bookmark-checkbox");
+const bookmarkLabel = document.querySelector(".bookmark-label");
+const bookmarkSpan = document.querySelector(".bookmark-label span");
+
+const backProjectBtn = document.querySelector(".back-project-btn");
+const selectionModal = document.querySelector(".selection-modal-overlay");
+const closeModalBtn = document.querySelector(".close-modal-btn");
+
+const pledges = document.querySelectorAll(".pledge");
+
+const successModal = document.querySelector(".success-modal-overlay");
+const successBtn = document.querySelector(".success-btn");
+
+// show mobile menu
 navToggle.addEventListener("click", function () {
   navDesktop.classList.toggle("show-menu");
 });
 
 // bookmarked
-const bookmark = document.querySelector(".bookmark-checkbox");
-const bookmarkLabel = document.querySelector(".bookmark-label");
-const bookmarkSpan = document.querySelector(".bookmark-label span");
 bookmark.addEventListener("click", function () {
   let bookmarkStatus = bookmark.checked;
   bookmarkStatus
@@ -19,23 +29,19 @@ bookmark.addEventListener("click", function () {
       (bookmarkSpan.textContent = "Bookmark"));
 });
 
-const backProjectBtn = document.querySelector(".back-project-btn");
-const closeModalBtn = document.querySelector(".close-modal-btn");
-const selectionModal = document.querySelector(".selection-modal-overlay");
-
 // toggle selection modal
 backProjectBtn.addEventListener("click", function () {
   selectionModal.classList.add("show-selection-modal");
+  let position = selectionModal.offsetTop + 160;
+  selectionModal.scrollTo({ top: position, left: 0, behavior: "smooth" });
 });
 
-// pledge
-const pledges = document.querySelectorAll(".pledge");
-const successModal = document.querySelector(".success-modal-overlay");
-const successBtn = document.querySelector(".success-btn");
-
 pledges.forEach((pledge) => {
-  // select individual pledge and show pledge value
+  const rewardSelections = document.querySelectorAll(".reward-selection");
   const selectPledge = pledge.querySelector(".select-pledge");
+  const pledgeSubmit = pledge.querySelector(".pledge-submit");
+
+  // select individual pledge and show pledge value
   selectPledge.addEventListener("click", function () {
     pledges.forEach((element) => {
       if (element !== pledge) {
@@ -46,7 +52,6 @@ pledges.forEach((pledge) => {
   });
 
   // select individual pledge when open selection modal
-  const rewardSelections = document.querySelectorAll(".reward-selection");
   rewardSelections.forEach((rewardSelection) => {
     const rewardBtn = rewardSelection.querySelector(".reward-selection button");
     const rewardLeft = rewardSelection.querySelector(".reward-left span");
@@ -55,48 +60,36 @@ pledges.forEach((pledge) => {
       if (pledge.classList.contains(rewardLeft.className)) {
         pledge.classList.add("toggle-pledge");
         selectPledge.checked = true;
+        let position = pledge.offsetTop - 150;
+        selectionModal.scrollTo({ top: position, left: 0, behavior: "smooth" });
       }
-    });
-
-    // close selection modal and reset pledge
-    closeModalBtn.addEventListener("click", function () {
-      selectionModal.classList.remove("show-selection-modal");
-      pledge.classList.remove("toggle-pledge");
-      selectPledge.checked = false;
     });
   });
 
   let pledgeNumberLeft = parseInt(
     pledge.querySelector(".pledge-number-left").textContent
   );
-  const pledgeSubmit = pledge.querySelector(".pledge-submit");
   pledgeSubmit.addEventListener("submit", function (e) {
     e.preventDefault();
-    // update pledge count
+
     if (isNaN(pledgeNumberLeft)) {
       pledge.querySelector(".pledge-number-left").textContent = "";
     } else {
       pledgeNumberLeft -= 1;
-      pledge.querySelector(".pledge-number-left").textContent =
-        pledgeNumberLeft;
+      pledge.querySelector(".pledge-number-left").textContent = pledgeNumberLeft;
     }
 
-    // update reward number
-    const rewardSelections = document.querySelectorAll(".reward-selection");
     rewardSelections.forEach((rewardSelection) => {
+      const reward = rewardSelection.parentElement;
       const rewardLeft = rewardSelection.querySelector(".reward-left span");
+      const rewardBtn = rewardSelection.querySelector(".reward-selection button");
+
+      // reduce pledge count
       if (pledge.classList.contains(rewardLeft.className)) {
         rewardLeft.textContent = pledgeNumberLeft;
       }
-    });
 
-    // disable reward button when reward count is 0
-    rewardSelections.forEach((rewardSelection) => {
-      const rewardBtn = rewardSelection.querySelector(
-        ".reward-selection button"
-      );
-      const rewardLeft = rewardSelection.querySelector(".reward-left span");
-      const reward = rewardSelection.parentElement;
+      // disable reward button when reward count is 0
       if (rewardLeft.textContent === "0") {
         rewardBtn.disabled = true;
         rewardBtn.textContent = "Out of Stock";
@@ -104,9 +97,8 @@ pledges.forEach((pledge) => {
       }
     });
 
-    // show success modal
-    successModal.classList.add("show-success-modal");
-    selectionModal.classList.remove("show-selection-modal");
+    //reset input value
+    pledgeSubmit.reset();
 
     // reset pledge
     pledge.classList.remove("toggle-pledge");
@@ -118,35 +110,42 @@ pledges.forEach((pledge) => {
       pledge.classList.add("disable-pledge");
     }
 
-    // update total backers number
+    const pledgeInput = pledge.querySelector(".pledge-input input");
     let targetNumber = document.querySelector(".target-number h1").textContent;
-    targetNumber = parseInt(targetNumber.replace(/,/g, ""));
-    targetNumber += 1;
-    document.querySelector(".target-number h1").textContent =
-      targetNumber.toLocaleString();
-
-    // update backed value and progress bar
-    let pledgeInput = pledge.querySelector(".pledge-input input");
-    let targetValue = document
-      .querySelector(".target-value h1")
-      .textContent.slice(1);
+    let targetValue = document.querySelector(".target-value h1").textContent.slice(1);
     let progress = document.querySelector("progress");
 
+    // update total backers number
+    targetNumber = parseInt(targetNumber.replace(/,/g, ""));
+    targetNumber += 1;
+    document.querySelector(".target-number h1").textContent = targetNumber.toLocaleString();
+
+    // update backed value
     if (pledgeInput !== null) {
       targetValue = parseInt(targetValue.replace(/,/g, ""));
       targetValue += parseInt(pledgeInput.value);
     } else {
       targetValue = parseInt(targetValue.replace(/,/g, ""));
     }
+    document.querySelector(".target-value h1").textContent = `$${targetValue.toLocaleString()}`;
 
-    document.querySelector(
-      ".target-value h1"
-    ).textContent = `$${targetValue.toLocaleString()}`;
-
+    // update progress bar
     progress.value = (targetValue / 100000) * 100;
     if (progress.value === 100) {
       progress.classList.add("full");
     }
+
+    // show success modal
+    successModal.classList.add("show-success-modal");
+    selectionModal.classList.remove("show-selection-modal");
+  });
+
+  // close selection modal and reset pledge
+  closeModalBtn.addEventListener("click", function () {
+    selectionModal.classList.remove("show-selection-modal");
+    pledge.classList.remove("toggle-pledge");
+    selectPledge.checked = false;
+    pledgeSubmit.reset();
   });
 });
 
